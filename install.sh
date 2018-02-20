0b33c53ca7af26edcf0347df97be16b523cdff62
Ariel <tager18@op.pl>
2018-02-21 00:20:42 +0100

    dodanie nowych plików

--- install.sh
+++ install.sh
@@ -1,4 +1,32 @@
-#!/bin/sh
-echo "Helo Word"
+#! /bin/sh
+echo "Move dot files"
+pwd
+
+dir="$HOME/dotfile"
+dir_old="$HOME/old_dotfile"
+
+#files=".bashrc .vimrc .zshrc .gitconfig .tmux.conf"
+files=".tmux.conf .vimrc"
+
+for file in $dir ; do 
+    echo $file
+done
+
+
+echo "Creating folder $dir_old"
+mkdir -pv "$dir_old"
+
+
+echo "Changing to the $dir directory"
+
+for file in $files; do
+    echo "Moving existing dotfiles from ~ to $dir_old"
+    mv -v "$HOME/$file" "$dir_old"
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
+
+echo "list of files"
+ls -al "$dir_old"
 
 exit 0
